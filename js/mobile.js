59cf429a83bddef3b02a0e76da48d768463c438b
jaav <jef@virtualsushi.be>
2014-01-27 11:17:31 +0100

    Complete rewrite of website Adding the 'go back' functionality Adding a menu to the mobile site

--- js/mobile.js
+++ js/mobile.js
@@ -2,24 +2,124 @@ var pair_styles = ["toycamera", "squared", "vintage2", "cartoon", "thermography"
 var unpair_styles = ["toycamera", "squared", "vintage2", "cartoon", "thermography"];
 var current_styles;
 var current_style = "thermography";
+var previousKey;
+var historyCounter = 10;
 $(document).ready(function () {
 
+	$("#nav")
+		.mmenu()
+	  .on(
+	     "closed.mm",
+	     function(a, b, c){
+		     if($('#about').parent().hasClass('mm-selected')){
+			     $('#about_container').css('display', 'block');
+			     $('html > body').animate({scrollTop:1000});
+			     $('#about').parent().removeClass('mm-selected');
+		     }
+	     }
+	  );
+	previousKey = pub_key;
+	doGetItems(pub_key);
 
-	$.getJSON('/api/ticks/' + pub_key, function (data) {
+
+	/*$('#about').click(function(ev){
+		ev.preventDefault();
+		//console.log("Before open. The height is "+$(document).height());
+		$('#about_container').css('display', 'block');
+		//console.log("Opened. Now the height is "+$(document).height());
+		$('html > body').animate({scrollTop:300}, 1000);
+		//unselect 'about' item in nav
+		$('#about').parent().removeClass('mm-selected');
+	});*/
+	$('#up').click(function(ev){
+		ev.preventDefault();
+		$('html, body').animate({scrollTop:0}, function(){
+			$('#about_container').css('display', 'none');
+		});
+	});
+	$('#share').click(function (e) {
+		e.preventDefault();
+		$(this).attr('href', "https://twitter.com/intent/tweet?text=Discovering 'Double Blind' showing the hottest tech news every quarter on the quarter - http://dblnd.com");
+		twttr.events.bind('tweet', function (ev) {});
+	});
+	$('#history').click(function (e) {
+		e.preventDefault();
+		historyCounter--;
+		if(historyCounter>0){
+			var newKey = getPreviousKey();
+			doGetItems(newKey);
+			previousKey = newKey;
+		}
+		else{
+			$('#historyEnd').modal()
+		}
+	});
+});
+
+
+var removeOldTicks = function(){
+	var ticks = $('div#mySwipe div.swipe-wrap div.tweet');
+	if(ticks.length > 5){
+		ticks.each(
+			function(index) {
+				if(index<5)
+			    $(this).remove();
+				else return;
+			});
+	}
+}
+
+var getPreviousKey = function(){
+	//var previousKey = '2014_01_20_04_Q4';
+	if(previousKey.indexOf('Q4')>0) return previousKey.substr(0, 15)+ '3';
+	if(previousKey.indexOf('Q3')>0) return previousKey.substr(0, 15)+ '2';
+	if(previousKey.indexOf('Q2')>0) return previousKey.substr(0, 15)+ '1';
+	if(previousKey.indexOf('Q1')>0){
+		var hour = parseInt(previousKey.substr(11, 2));
+		if(hour>0 && hour<11) return previousKey.substr(0, 11)+ '0' +(hour-1)+ '_Q4';
+		else if(hour > 10) return previousKey.substr(0, 11)+ (hour-1)+ '_Q4';
+		else{
+			var today = new Date();
+			today.setDate(today.getDate() - 1);
+			var dateString = today.format("yyyy_mm_dd_");
+				return(dateString+'23_Q4');
+		}
+
+	}
+}
+
+var doGetItems = function(key){
+	$.getJSON('/api/ticks/' + key, function (data) {
 		var items = [];
 
-		var enrichTweet = function (tweetContent, hashtags) {
-			var myregex = /https?:\/\/[^\s]*/g;
-			tweetContent = tweetContent.replace(myregex, ' ');
-			if (hashtags != undefined && hashtags.length > 0) {
-				for (var i = 0; i < hashtags.length; i++) {
-					var tag = hashtags[i];
-					var regex = new RegExp('#' + tag);
-					tweetContent = tweetContent.replace(regex, '<a href="https://twitter.com/search?q=%23' + tag + '" target="_blank">#' + tag + '</a>');
-				}
+		var enrichTweet = function (tweetContent, urls) {
+			if (urls != undefined && urls.length > 0) {
+				tweetContent = replaceLinks(tweetContent, urls);
 			}
+			tweetContent = replaceHashtagsWithLinks(tweetContent);
+			tweetContent = replaceMentionsWithLinks(tweetContent);
+			if(tweetContent.indexOf('"')==0) return tweetContent;
+			else return '"'+tweetContent+'"';
+		}
 
-			return tweetContent;
+		var replaceLinks = function(tweetContent, urls){
+			var counter = -1;
+			return tweetContent.replace(/https?:\/\/[^\s]*/g, function(i, match, c, d) {
+					counter++;
+					if(urls.length > counter){
+						var tiny_url = urls[counter].substr(0, urls[counter].indexOf('/', 8))+'/...';
+				    return '<a href="'+urls[counter]+'" target="_blank">'+tiny_url+'</a>';
+					}
+					else return '';
+			});
+		}
+
+		var replaceMentionsWithLinks = function( text ) {
+		    return text.replace(/@([a-z\d_]+)/ig, '<a href="https://mobile.twitter.com/$1" target="_blank">@$1</a>');
+		}
+
+		var replaceHashtagsWithLinks = function( text ) {
+		    return text.replace(/\s#([a-z\d_]+)/ig, ' <a href="https://mobile.twitter.com/search?q=%23$1" target="_blank">#$1</a>');
 		}
 
 		var getFilter = function (path) {
@@ -27,12 +127,10 @@ $(document).ready(function () {
 			var result = myRe.exec(path)[0];
 			if (result) return result.substring(1, result.length - 1);
 		}
-		var about = 'We constantly scan all tweets published by a panel of about 2000 multidisciplinary techies, mostly software developers from all over the world. Every 15 minutes on the 15th minute, we filter out the 5 most important/popular entries and publish them here and on our <a href="">android app</a> (iOS is underway).';
-
 		$.each(data.tweets, function (key, val) {
 			if (val.id.indexOf("Q2") > 0 || val.id.indexOf("Q4") > 0) current_styles = pair_styles;
 			else current_styles = unpair_styles;
-			var enrichedTweet = enrichTweet(val.tweet, val.hashtags);
+			var enrichedTweet = enrichTweet(val.tweet, val.urls);
 			/*var tags = [];
 			 if(val.hashtags!=undefined && val.hashtags.length>0){
 			 $.each(val.hashtags, function(i, el){
@@ -51,10 +149,7 @@ $(document).ready(function () {
 				});
 			}
 			if (val.image == null || val.image.length == 'undefined') val.image = 'no_image';
-			var httpurls = "";
-			if (urls.length > 0)
-				httpurls = '<div class="urls">' + urls.join(" - ") + '</div>';
-			items.push('<div class="tweet"><img class="real" src="/repo/datatracker_images/' + val.image + '_cartoon.jpg"><img src="../images/template_w_b.png" class="border" /><div class="about"><a href="#"><img src="../images/db_sq_logo_32.png" /></a></div><div class="retweet"><a href="twitter://post?message='+encodeURIComponent(val.tweet)+' - via @'+val.author+' and @dblnd'+'"><img src="../images/share.png" /></a></div><div class="content_container"><div class="content">\"' + enrichedTweet + '\"</div>' + httpurls + '<div class="author"><a href="http://twitter.com/#!/' + val.author + '" target="_blank">@' + val.author + '</a></div></div><div class="about_container"><div class="about_header">Welcome to \'Double Blind\'</div><div class="about_content">'+about+'</div><div class="up"><a href="#"><img src="../images/up_32.png" /></a></div></div></div>');
+			items.push('<div class="tweet"><img class="real" src="/repo/datatracker_images/' + val.image + '_cartoon.jpg"><img src="../images/template_w_b.png" class="border" /><div class="retweet"><a href="https://twitter.com/intent/tweet?text='+encodeURIComponent(val.tweet)+' - via @'+val.author+' and @dblnd'+'"><img src="../images/share.png" /></a></div><div class="content_container"><div class="content">' + enrichedTweet + '</div><div class="author"><a href="http://twitter.com/#!/' + val.author + '" target="_blank">@' + val.author + '</a></div></div></div>');
 		});
 
 		$(items.join('')).appendTo('div#mySwipe div.swipe-wrap');
@@ -71,23 +166,20 @@ $(document).ready(function () {
 				thisImage.attr('src', newSrc);
 			});
 		});
-
+		removeOldTicks();
 		window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
 
-		$('.about').click(function(ev){
-			ev.preventDefault();
-			$('.about_header').show();
-			$('.about_content').show();
-			$('.up').show();
-			$('html, body').animate({scrollTop:$(document).height()});
-		});
-		$('.up').click(function(ev){
-			ev.preventDefault();
-			$('html, body').animate({scrollTop:0}, function(){
-				$('.about_header').hide();
-				$('.about_content').hide();
-				$('.up').hide();
-			});
-		});
 	});
-});
+}
+
+window.twttr = (function (d, s, id) {
+	var t, js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//platform.twitter.com/widgets.js";
+	fjs.parentNode.insertBefore(js, fjs);
+	return window.twttr || (t = { _e: [], ready: function (f) {
+		t._e.push(f);
+	} });
+}(document, "script", "twitter-wjs"));
--- js/tick5.js
+++ js/tick5.js
@@ -2,37 +2,135 @@ var pair_styles = ["toycamera", "squared", "vintage2", "cartoon", "thermography"
 var unpair_styles = ["toycamera", "squared", "vintage2", "cartoon", "thermography"];
 var current_styles;
 var current_style = "thermography";
+var previousKey;
+var historyCounter = 10;
 $(document).ready(function () {
 
+	previousKey = pub_key;
+	doGetItems(pub_key);
+	/*$("img.logo").click(function (ev) {
+		ev.preventDefault();
+		$(".title_container").animate({
+			top: -20
+		}, 400, function () {
+			$('#dblnd').modal()
+		});
+	});*/
+	/*$('#dblnd').on('hidden.bs.modal', function () {
+		$(".title_container").animate({
+			top: -336
+		}, 400);
+	});*/
+	$('#footer a img').mouseover(function(ev){
+		ev.preventDefault();
+		$(this).animate({
+			width: 32,
+			marginTop: -12
+		}, 200);
+		$(this).parent().animate({
+			marginRight: 8
+		}, 200);
+	});
+	$('#footer a img').mouseout(function(ev){
+		ev.preventDefault();
+		$(this).animate({
+			width: 20,
+			marginTop: 0
+		}, 200);
+		$(this).parent().animate({
+			marginRight: 20
+		}, 200);
+	});
+	$('#share').click(function (e) {
+		e.preventDefault();
+		$(this).attr('href', "https://twitter.com/intent/tweet?text=Discovering 'Double Blind' showing the hottest tech news every quarter on the quarter - http://dblnd.com");
+		twttr.events.bind('tweet', function (ev) {});
+	});
+	$('#history').click(function (e) {
+		e.preventDefault();
+		historyCounter--;
+		if(historyCounter>0){
+			var newKey = getPreviousKey();
+			doGetItems(newKey);
+			previousKey = newKey;
+			removeOldTicks();
+		}
+		else{
+			doGetItems(pub_key);
+			previousKey = pub_key;
+			removeOldTicks();
+			$('#historyEnd').modal();
+		}
+	});
+});
 
-	$.getJSON('/api/ticks/' + pub_key, function (data) {
+var removeOldTicks = function(){
+	$('div#makeMeScrollable div.scrollWrapper div.scrollableArea div.tweet').each(function(index) {
+		if(index<5)
+	    $(this).remove();
+		else return;
+	});
+}
+
+var getPreviousKey = function(){
+	//var previousKey = '2014_01_20_04_Q4';
+	if(previousKey.indexOf('Q4')>0) return previousKey.substr(0, 15)+ '3';
+	if(previousKey.indexOf('Q3')>0) return previousKey.substr(0, 15)+ '2';
+	if(previousKey.indexOf('Q2')>0) return previousKey.substr(0, 15)+ '1';
+	if(previousKey.indexOf('Q1')>0){
+		var hour = parseInt(previousKey.substr(11, 2));
+		if(hour>0 && hour<11) return previousKey.substr(0, 11)+ '0' +(hour-1)+ '_Q4';
+		else if(hour > 10) return previousKey.substr(0, 11)+ (hour-1)+ '_Q4';
+		else{
+			var today = new Date();
+			today.setDate(today.getDate() - 1);
+			var dateString = today.format("yyyy_mm_dd_");
+				return(dateString+'23_Q4');
+		}
+
+	}
+}
+
+
+var doGetItems = function(key){
+	$.getJSON('/api/ticks/' + key, function (data) {
 		var items = [];
 
-		var enrichTweet = function (tweetContent, hashtags) {
-			var myregex = /https?:\/\/[^\s]*/g;
-			tweetContent = tweetContent.replace(myregex, ' ');
-			if (hashtags != undefined && hashtags.length > 0) {
-				for (var i = 0; i < hashtags.length; i++) {
-					var tag = hashtags[i];
-					var regex = new RegExp('#' + tag);
-					tweetContent = tweetContent.replace(regex, '<a href="https://twitter.com/search?q=%23' + tag + '" target="_blank">#' + tag + '</a>');
-				}
+		var enrichTweet = function (tweetContent, urls, pics) {
+			if (urls != undefined && urls.length > 0) {
+				tweetContent = replaceLinks(tweetContent, urls);
 			}
+			tweetContent = replaceHashtagsWithLinks(tweetContent);
+			tweetContent = replaceMentionsWithLinks(tweetContent);
+			if(tweetContent.indexOf('"')==0) return tweetContent;
+			else return '"'+tweetContent+'"';
+		}
 
-			return tweetContent;
+		var replaceLinks = function(tweetContent, urls){
+			var counter = -1;
+			return tweetContent.replace(/https?:\/\/[^\s]*/g, function(i, match, c, d) {
+					counter++;
+					if(urls.length > counter){
+						var tiny_url = urls[counter].substr(0, urls[counter].indexOf('/', 8))+'/...';
+				    return '<a href="'+urls[counter]+'" target="_blank">'+tiny_url+'</a>';
+					}
+					else return '';
+			});
+		}
+
+		var replaceMentionsWithLinks = function( text ) {
+		    return text.replace(/@([a-z\d_]+)/ig, '<a href="http://twitter.com/$1" target="_blank">@$1</a>');
+		}
+
+		var replaceHashtagsWithLinks = function( text ) {
+		    return text.replace(/\s#([a-z\d_]+)/ig, ' <a href="https://twitter.com/search?q=%23$1" target="_blank">#$1</a>');
 		}
 
 
 		$.each(data.tweets, function (key, val) {
 			if (val.id.indexOf("Q2") > 0 || val.id.indexOf("Q4") > 0) current_styles = pair_styles;
 			else current_styles = unpair_styles;
-			var enrichedTweet = enrichTweet(val.tweet, val.hashtags);
-			/*var tags = [];
-			 if(val.hashtags!=undefined && val.hashtags.length>0){
-			 $.each(val.hashtags, function(i, el){
-			 tags.push('<a href="https://twitter.com/search?q=%23'+el+'" target="_blank">#'+el+'</a>');
-			 });
-			 }*/
+			var enrichedTweet = enrichTweet(val.tweet, val.urls);
 			var urls = [];
 			if (val.urls != undefined && val.urls.length > 0) {
 				$.each(val.urls, function (i, el) {
@@ -45,11 +143,11 @@ $(document).ready(function () {
 				});
 			}
 			if (val.image == null || val.image.length == 'undefined') val.image = 'no_image';
-			var httpurls = "";
+			/*var httpurls = "";
 			if (urls.length > 0)
-				httpurls = '<div class="urls">' + urls.join(" - ") + '</div>';
-				items.push('<div class="tweet"><img class="real" src="/repo/datatracker_images/' + val.image + '_' + current_styles[0] + '.jpg" width="460px"><img src="images/template_w.png" width="480" style="position:absolute;top:0;left:0;" /><div class="content_container"><div class="content">\"' + enrichedTweet + '\"</div>' + httpurls + '<div class="retweet"><a href="https://twitter.com/intent/tweet?text='+encodeURIComponent(val.tweet)+' - via @'+val.author+' and @dblnd'+'"><img src="../images/share.png" /></a></div><div class="author"><a href="http://twitter.com/#!/' + val.author + '" target="_blank">@' + val.author + '</a></div> </div></div > ');
-			});
+				httpurls = '<div class="urls">' + urls.join(" - ") + '</div>';*/
+			items.push('<div class="tweet"><img class="real" src="/repo/datatracker_images/' + val.image + '_' + current_styles[0] + '.jpg" width="420px"><img src="images/template_w.png" width="420" style="position:absolute;top:0;left:0;" /><div class="content_container"><div class="content">' + enrichedTweet + '</div><div class="retweet"><a href="https://twitter.com/intent/tweet?text='+encodeURIComponent(val.tweet)+' - via @'+val.author+' and @dblnd'+'"><img src="images/share.png" /></a></div><div class="author"><a href="http://twitter.com/#!/' + val.author + '" target="_blank">@' + val.author + '</a></div> </div></div > ');
+		});
 
 		$(items.join('')).appendTo('div#makeMeScrollable div.scrollWrapper div.scrollableArea');
 		$("div#makeMeScrollable").smoothDivScroll({
@@ -77,28 +175,7 @@ $(document).ready(function () {
 			});
 		});
 	});
-	$("img.logo").click(function (ev) {
-		ev.preventDefault();
-		$(".title_container").animate({
-			top: -20
-		}, 400, function () {
-			$('#dblnd').modal()
-		});
-	});
-	$('#dblnd').on('hidden.bs.modal', function () {
-		$(".title_container").animate({
-			top: -336
-		}, 400);
-	});
-	$('#share').click(function (e) {
-		e.preventDefault();
-		$(this).attr('href', "https://twitter.com/intent/tweet?text=Discovering 'Double Blind' showing the hottest tech news every quarter on the quarter - http://dblnd.com");
-		twttr.events.bind('tweet', function (ev) {
-			alert('tweet');
-		});
-
-	});
-});
+}
 
 window.twttr = (function (d, s, id) {
 	var t, js, fjs = d.getElementsByTagName(s)[0];
